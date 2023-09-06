7852f6baf2869bbb7fb17890326e353d1821e578
Isaac McIntosh <issi.mc@gmail.com>
2023-09-06 17:58:27 +0100

    AIsnake filename and class name changed
    removed the hyphon which I'm pretty sure was causing compile issues on the linux laptop

--- Assets/Scripts/AIsnake.cs
+++ Assets/Scripts/AIsnake.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AI-snake : MonoBehaviour
+public class AIsnake : MonoBehaviour
 {
     void Start()
     {
